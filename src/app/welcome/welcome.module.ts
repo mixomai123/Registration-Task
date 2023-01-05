a440278a26193c4888a593d1b96c10f44891016b
mixomai123 <mixo96@yahoo.de>
2023-01-05 14:42:24 +0400

    refactor(file-structure): flatten main components inside feature-modules

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from './welcome.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const ROUTES: Routes = [
