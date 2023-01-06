0f153c6a5aaae89eb987737c78a07378f38e02c4
mixomai123 <mixo96@yahoo.de>
2023-01-06 10:53:46 +0400

    style: add dashboard heading

--- src/app/registration/registration.service.spec.ts
+++ src/app/registration/registration.service.spec.ts
@@ -1,7 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { RegistrationService } from './registration.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('RegistrationService', () => {
   let service: RegistrationService;
@@ -14,4 +17,16 @@ describe('RegistrationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getRegistrationForm fields from /registrationFields', inject(
+    [HttpTestingController],
+    (httpMock: HttpTestingController) => {
+      const service = TestBed.inject(RegistrationService);
+      service.getRegistrationFormFields().subscribe();
+
+      const req = httpMock.expectOne('/registrationFields');
+      expect(req.request.method).toEqual('GET');
+      httpMock.verify();
+    }
+  ));
 });
